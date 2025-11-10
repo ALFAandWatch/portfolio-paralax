0a1562c27aa5834a7341129f9d7b7ce589e18f0c
Alfonso <alfonso.gonzalezwebdev@gmail.com>
2025-11-10 14:16:25 -0300

    CVs y botones corregidos

--- components/CVButton/CVButton.tsx
+++ components/CVButton/CVButton.tsx
@@ -2,27 +2,39 @@
 import Image from 'next/image';
 
 const CVButton = () => {
-   const handleDownload = () => {
-      if (confirm('¿Deseás descargar tu archivo CV?')) {
-         window.location.href =
-            '/resume-alfonso-gonzalez-fullstack-developer.zip';
-      }
-   };
-
    return (
-      <button
-         onClick={handleDownload}
-         className="w-auto h-12 px-5 text-gray-300 border border-gray-300 hover:bg-violet-500 flex justify-center items-center rounded-full transition-colors duration-300 gap-2"
-      >
-         Mi CV
-         <Image
-            src="/download.png"
-            alt="Descargar CV"
-            width={30}
-            height={30}
-            className="inline-block"
-         />
-      </button>
+      <div className="flex gap-4">
+         <a
+            href="/resume_alfonso_gonzalez_fullstackDeveloper.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-auto h-12 px-5 text-gray-300 border border-gray-300 hover:bg-violet-500 flex justify-center items-center rounded-full transition-colors duration-300 gap-2"
+         >
+            Mi CV (ES)
+            <Image
+               src="/download.png"
+               alt="Descargar CV"
+               width={30}
+               height={30}
+               className="inline-block"
+            />
+         </a>
+         <a
+            href="/resume_alfonso_gonzalez_fullstackDeveloper_(ENG).pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-auto h-12 px-5 text-gray-300 border border-gray-300 hover:bg-violet-500 flex justify-center items-center rounded-full transition-colors duration-300 gap-2"
+         >
+            Mi CV (EN)
+            <Image
+               src="/download.png"
+               alt="Descargar CV"
+               width={30}
+               height={30}
+               className="inline-block"
+            />
+         </a>
+      </div>
    );
 };
 
